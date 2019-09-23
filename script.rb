a9c2d7830fbfa63ef26dc641aefb5c1749142090
Rohan Koripalli <rkoripalli@hotmail.com>
2019-09-23 15:48:19 -0500

    Initial commit. Still using wrong weather file.

---
+++ script.rb
@@ -0,0 +1,26 @@
+require 'pathname'
+require 'json'
+
+workflow_path = Pathname.new("workflows")
+weather_path = Pathname.new("weather_files")
+run_path = Pathname.new("run")
+output_path = Pathname.new("output")
+
+json = File.read(workflow_path+'workflow.osw')
+obj = JSON.parse(json)
+
+for i in Dir.glob(weather_path+'*.epw')
+    filename = File.basename(i)
+    basename = File.basename(i, ".epw")
+    obj['weather_file'] = "weather_files/#{filename}"
+    json = obj.to_json
+    workflow_name = Pathname.new(workflow_path + basename).sub_ext('.osw')
+    File.open(workflow_name, 'w') {
+        |file| file.write(json)
+    }
+    run_cmd = "openstudio run --workflow #{workflow_name}"
+
+    puts run_cmd
+    system("#{run_cmd}")
+    File.rename(run_path+'eplusout.sql', output_path + "eplusout#{basename}.sql")
+end
